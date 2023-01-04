ab96991a6f94e3fa861975a1220637f3046ee145
baejunil <bjo6300@naver.com>
2023-01-04 15:32:20 +0900

    feat : 회원 도메인 구현 및 테스트코드 작성

---
+++ src/main/java/hello/core/Member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 ConcurrentHashMap을 써서 동시성 이슈 해결
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
