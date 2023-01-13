698cef4bfd28ea4d27035c0742c81a43fd8c6729
baejunil <bjo6300@naver.com>
2023-01-13 15:04:56 +0900

    feat : 새로운 할인 정책 개발, AppConfig를 이용한 관심사의 분리

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -9,9 +9,11 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
-        long memberId = 1L;
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
