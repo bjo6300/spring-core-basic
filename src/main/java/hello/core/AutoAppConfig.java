7d2c9e330f2fd70b4f5c03e7393bf88cd14f1e50
baejunil <bjo6300@naver.com>
2023-02-02 21:43:04 +0900

    feat : 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 컴포넌트 스캔의 대상
+        basePackageClasses = AutoAppConfig.class, 
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 제외
 ) // @Component 붙은 코드를 자동으로 bean에 등록
 public class AutoAppConfig {
